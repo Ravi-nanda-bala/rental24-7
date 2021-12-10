5df11ac41a8d012c3ffe01b28f48c76e7bc1259d
Ravi-nanda-bala <ruderavi1998@gmail.com>
2021-12-10 18:44:10 +0530

    first commit

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -25,8 +25,8 @@ function Register() {
            className='w-100'
            data-aos='slide-left'
            data-aos-duration='1500'
-          src="https://images.unsplash.com/photo-1485291571150-772bcfc10da5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=928&q=80" />
-          <h1 className="login-logo">Cars For U</h1>
+          src="https://media.adtorqueedge.com/new-cars/nissan-aus/gtr/models/nismo.png" />
+          <h1 className="login-logo"></h1>
         </Col>
         <Col lg={8} className="text-left p-5">
           <Form layout="vertical" className="login-form p-5" onFinish={onFinish}>
--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 function connectDB(){
 
-    mongoose.connect('mongodb+srv://ravi:ravi1998@cluster0.eylp9.mongodb.net/car?retryWrites=true&w=majority' , {useUnifiedTopology: true , useNewUrlParser: true})
+    mongoose.connect('mongodb+srv://surya:098098@cluster0.cemih.mongodb.net/sheycars-udemy' , {useUnifiedTopology: true , useNewUrlParser: true})
 
     const connection = mongoose.connection
 
